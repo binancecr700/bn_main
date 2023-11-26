e045ed53f79df30da06fd23ac413199a53942488
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-11-26 13:02:19 +0100

    updated

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from home import views
 
 urlpatterns = [
     path('', views.home),
+    path('contact', views.contact),
 ]
--- home/views.py
+++ home/views.py
@@ -8,3 +8,8 @@ from django.views.decorators.csrf import csrf_exempt
 def home(request, *args, **kwargs):
     return render(request, 'home/home.html', {})
 
+
+@csrf_exempt
+def contact(request, *args, **kwargs):
+    return render(request, 'home/contact.html', {})
+
